d996401e81f22de1436f191eb1640a3215cf4323
rumiani <maziar.rumiani@gmail.com>
2023-04-30 21:16:26 +0100

    handeling 404

--- src/routers/joke.js
+++ src/routers/joke.js
@@ -5,6 +5,7 @@ const router = new express.Router()
 
 router.get('/:id', async (req, res) =>{
     const id = req.params.id
+    console.log(id);
     let jokes;
     try {
         if(ObjectId.isValid(id)){
@@ -21,5 +22,6 @@ router.get('/:id', async (req, res) =>{
         res.status(500).send({error:'Internal Server Error'})
     }
 })
+router.get('/', (req, res) => res.status(404).send({Tip:'Add an id or a number to the url and hit enter to see the result. Example: url/5'}) )
 
 module.exports = router
