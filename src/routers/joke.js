c2249442b94330f579467aa417fec109697ec3c8
rumiani <maziar.rumiani@gmail.com>
2023-04-30 21:48:12 +0100

    fixing typeos

--- src/routers/joke.js
+++ src/routers/joke.js
@@ -5,23 +5,26 @@ const router = new express.Router()
 
 router.get('/:id', async (req, res) =>{
     const id = req.params.id
-    console.log(id);
     let jokes;
     try {
         if(ObjectId.isValid(id)){
-            jokes = await Joke.findById('644ead287803a3117a0a6cab')
+            jokes = await Joke.findById(id)
             res.send({jokes})   
         }
         else if (id > 0 && id <= 10){
             jokes = await Joke.aggregate().sample(+id)
             res.send({jokes})
         }
-        else 
+        else {
             res.status(400).send({error:'Invalid params id. Only 12-byte object id or a numbers between 1-10 are acceptable'})
+        }
     } catch (error) {
         res.status(500).send({error:'Internal Server Error'})
     }
 })
-router.get('/', (req, res) => res.status(404).send({Tip:'Add an id or a number to the url and hit enter to see the result. Example: url/5'}) )
+
+router.get('/', (req, res) => {
+    res.status(404).send({Tip:'Add an id or a number to the url and hit enter to see the result. Example: url/5'})
+})
 
 module.exports = router
