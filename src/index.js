10290892bfd596c6a04c3d3988b3f3ab2b7c953a
rumiani <maziar.rumiani@gmail.com>
2023-04-30 02:55:25 +0100

    removing extra lines

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const app = require('./app')
+const app = require("./app")
 const port = process.env.PORT
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
 }).then(()=> {
-    app.listen(port, () => console.log('Server is up on port ' + port))
-    console.log('Connected to DB')})
+    app.listen(port, () => console.log("Server is up on port " + port))
+    console.log("Connected to DB")})
 
